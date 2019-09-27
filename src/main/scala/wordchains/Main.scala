db99701a8156adc18bc24ab712302bc88f16fa8c
jpatton638 <jpatton638@gmail.com>
2019-09-27 14:18:43 +0100

    Implemented working version of Word Chain

--- src/main/scala/wordchains/Main.scala
+++ src/main/scala/wordchains/Main.scala
@@ -5,13 +5,18 @@ object Main extends App {
 
   val lines = Source.fromResource("popular.txt").getLines().toList
 
-
-
+  WordChain.run("cat", "dog", lines).foreach(println(_))
 }
 
 object WordChain {
+
   def hammingDistance(str1: String, str2: String): Int = {
-    str1.zip(str2).filterNot{case (c1,c2) => c1==c2}.size
+
+    val difference = (str1.length - str2.length).abs
+
+    str1.zip(str2).filterNot{
+      case (c1,c2) => c1==c2
+    }.size + difference
 
   }
 
@@ -19,21 +24,18 @@ object WordChain {
     if(!dictionary.contains(word1) || !dictionary.contains(word2)) {
       Nil
     }
-    else if(word1.size != word2.size) {
+    else if(word1.length != word2.length) {
       Nil
     }
     else if (word1 == word2){
       List(word1)
-    }
-    else  {
-      val updatedWord =word1.updated(0,word2(0))
-      if(dictionary.contains(updatedWord)){
-        List(word1, updatedWord)
-      }
+    } else {
+      val newWord = dictionary.filter { w =>
+        hammingDistance(word1, w) == 1 &&
+        hammingDistance(word1, word2) > hammingDistance(word2, w)
+      }.head
 
-
-
-      else Nil
+      word1 :: run(newWord, word2, dictionary)
     }
   }
 }
